c6f326b4e0d56382f574b787cac999ca9b99f8c9
jchen49gsu <jchen49gsu@gmail.com>
2017-08-27 21:26:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/company/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.company;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.icu.text.NumberFormat;
+
+import com.example.android.company.R;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    int quantity = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
